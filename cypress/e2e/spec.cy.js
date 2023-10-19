7e6ea28da24ff97fc30f00cd1c0259c18b50c93d
2i-Gyorgy <Gyorgy.Papp@2itesting.com>
2023-10-19 13:51:39 +0100

    third excersise

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -22,17 +22,31 @@ describe('template spec', () => {
   //   cy.contains('File is ready!')
   // })
 
-  it('User log in generates data and save to the history', () => {
+  // it('User log in generates data and save to the history', () => {
+  //   cy.get('[href="/data"]').click()
+  //   cy.get('[id="templates-selector"]').select('Banking')
+  //   cy.get('[id="submit-template"]').click()
+  //   cy.get('[id="generate-values"]').click()
+  //   cy.contains('File is ready!')
+  //   cy.get('[id="file-name-input"]').invoke('val').then(sometext =>{ cy.log(sometext)
+  //   cy.get('[id="upload-button"]').click()
+  //   cy.get('[id="modal-ok-button"]').click()
+  //   cy.wait(3000)
+  //   cy.get('[href="/history"]').click()
+  //   cy.contains(sometext)})
+  // })
+
+  it('User logs into TDG and generates 100 data examples for "firstName", "lastName", "email", "fullAddress"', () => {
     cy.get('[href="/data"]').click()
-    cy.get('[id="templates-selector"]').select('Banking')
-    cy.get('[id="submit-template"]').click()
+    cy.get('[id="entries-counter"]').clear().type(100)
+    cy.get('[id="personal"]').click()
+    cy.contains('First name').click();
+    cy.contains('Last Name').click();
+    cy.contains('Email Address').click();
+    cy.get('[id="residentialAddress"]').click()
+    cy.contains('Full Address').click();
+    cy.get('[id="submit-selected"]').click()
     cy.get('[id="generate-values"]').click()
     cy.contains('File is ready!')
-    cy.get('[id="file-name-input"]').invoke('val').then(sometext =>{ cy.log(sometext)
-    cy.get('[id="upload-button"]').click()
-    cy.get('[id="modal-ok-button"]').click()
-    cy.wait(3000)
-    cy.get('[href="/history"]').click()
-    cy.contains(sometext)})
   })
 })
