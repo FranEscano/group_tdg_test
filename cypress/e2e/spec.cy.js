3b89a60709dba7045ed7b850a98a3de7fd4a5d9c
Fran Bejarano Escano <147075385+FranEscano@users.noreply.github.com>
2023-10-19 12:26:50 +0100

    second exercise

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,10 +2,10 @@ describe('template spec', () => {
 
   beforeEach(() => {
     cy.visit('https://develop.d3nylssqqiptjw.amplifyapp.com/')
-    cy.get('[id="email-input"]').type('joebloggs@gmail.com')
-    cy.get('[id="password-input"]').type('123456')
-    cy.get('[id="login-button"]').click()
-    cy.contains('Data')
+    // cy.get('[id="email-input"]').type('team1@test.com')
+    // cy.get('[id="password-input"]').type('123456')
+    // cy.get('[id="login-button"]').click()
+    // cy.contains('Data')
   })
 
   // afterEach(() => {
@@ -13,14 +13,26 @@ describe('template spec', () => {
   // })
 
   
-  it('logs in', () => {
+  // it('logs in', () => {
     
-    
+  //   cy.get('[href="/data"]').click()
+  //   cy.get('[id="templates-selector"]').select('Banking')
+  //   cy.contains('Submit Banking').click()
+  //   cy.get('[id="generate-values"]').click()
+  //   cy.contains('File is ready!')
+  // })
+
+  it('User log in generates data and save to the history', () => {
     cy.get('[href="/data"]').click()
     cy.get('[id="templates-selector"]').select('Banking')
-    cy.contains('Submit Banking').click()
+    cy.get('[id="submit-template"]').click()
     cy.get('[id="generate-values"]').click()
     cy.contains('File is ready!')
+    cy.get('[id="file-name-input"]').invoke('val').then(sometext =>{ cy.log(sometext)
+    cy.get('[id="upload-button"]').click()
+    cy.get('[id="modal-ok-button"]').click()
+    cy.wait(3000)
+    cy.get('[href="/history"]').click()
+    cy.contains(sometext)})
   })
-
 })
